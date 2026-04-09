8deff12fd9bf70cdf8649109940cb446136af546
Revashan <141326969+Revashan@users.noreply.github.com>
2026-04-09 13:11:22 +0800

    Update tablecreation.sql

--- sql/tablecreation.sql
+++ sql/tablecreation.sql
@@ -1,12 +1,1 @@
-/*TABLE CREATION*/
-    
-CREATE TABLE ecommerce_data (
-    customer_id VARCHAR(50),
-    customer_city VARCHAR(50),
-    customer_state VARCHAR(50),
-    order_id VARCHAR(50),
-    order_date DATE,
-    product_category VARCHAR(100),
-    price FLOAT,
-    payment_value FLOAT
-);
+
